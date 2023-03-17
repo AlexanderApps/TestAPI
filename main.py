d956bc9630befd314c4803c7a8c073461c83938e
AlexanderApps <74374090+AlexanderApps@users.noreply.github.com>
2023-03-17 20:15:14 +0000

    pycache changes

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db_actions.manage_tables import create_tables, drop_tables, init_tables
 from routers import login_router, product_router, \
-      transaction_router, user_router, product_category_router
+    transaction_router, user_router, product_category_router
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
 
 
 ###############################################
@@ -11,13 +18,21 @@ from routers import login_router, product_router, \
 # init_tables()
 
 
-
 ##################
 # drop_tables()
 ###############################################
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
@@ -29,5 +44,3 @@ app.include_router(user_router.router)
 app.include_router(product_router.router)
 app.include_router(product_category_router.router)
 app.include_router(transaction_router.router)
-
-
