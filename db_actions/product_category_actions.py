1f6ce886490596919369b9827cf65aac2ec68422
AlexanderApps <alexanderapps6699@gmail.com>
2023-03-20 01:23:42 +0000

    added filter to product category

--- db_actions/product_category_actions.py
+++ db_actions/product_category_actions.py
@@ -3,11 +3,16 @@ from models.product import Product
 from helper.sort_order_mapper import mapper
 from models.product_category import ProductCategory
 from models.product_category_ref import ProductCategoryRef
-from schemas.iproduct_category import IProductCategory, IProductCategoryQueryParams, IProductCategoryRef, IProductCategoryUpdate
+from schemas.iproduct import IProductQueryParams
+from schemas.iproduct_category import (
+    IProductCategory,
+    IProductCategoryQueryParams,
+    IProductCategoryRef,
+    IProductCategoryUpdate,
+)
 
 
 class ProductCategoryActions:
-
     @staticmethod
     def create_category(category: IProductCategory, current_user: int):
         category_ = category.dict().copy()
@@ -26,10 +31,9 @@ class ProductCategoryActions:
         rows = (
             ProductCategory.select()
             .where(
-                ProductCategory
-                .category_name
-                .contains(params.name)
-                if params.name else True
+                ProductCategory.category_name.contains(params.name)
+                if params.name
+                else True
             )
             .order_by(*sort_by)
             .limit(params.limit)
@@ -42,9 +46,7 @@ class ProductCategoryActions:
     @staticmethod
     def get_category_by_id(id_: int):
         rows = (
-            ProductCategory.select().where(
-                ProductCategory.category_id == id_
-            ).dicts()
+            ProductCategory.select().where(ProductCategory.category_id == id_).dicts()
         )
         category = [row for row in rows]
         try:
@@ -54,8 +56,11 @@ class ProductCategoryActions:
 
     @staticmethod
     def get_category_by_name(name: str):
-        rows = ProductCategory.select().where(
-            ProductCategory.category_name == name).dicts()
+        rows = (
+            ProductCategory.select()
+            .where(ProductCategory.category_name == name)
+            .dicts()
+        )
         return [row for row in rows]
 
     @staticmethod
@@ -65,17 +70,21 @@ class ProductCategoryActions:
         return category
 
     @staticmethod
-
-    def update_category(category_id: int, category: IProductCategoryUpdate, current_user: int):
-        category_ = {x: y for x, y in category.dict().items()
-                     if y != None}.copy()
-        ProductCategory.update(
-            **category_).where(ProductCategory.category_id == category_id).execute()
+    def update_category(
+        category_id: int, category: IProductCategoryUpdate, current_user: int
+    ):
+        category_ = {x: y for x, y in category.dict().items() if y != None}.copy()
+        ProductCategory.update(**category_).where(
+            ProductCategory.category_id == category_id
+        ).execute()
         return ProductCategoryActions.get_category_by_id(category_id)
 
     @staticmethod
-    def get_category_items(category_id: int, current_user: int):
-        # rows = ProductCategoryRef.select().where(ProductCategoryRef.category_id == category_id).dicts()
+    def get_category_items(
+        category_id: int, current_user: int, params: IProductQueryParams
+    ):
+        sb = mapper(Product, params.sort_by, params.order)
+        sort_by = sb if sb else [Product.product_id]
         __rows = (
             ProductCategoryRef.select(
                 ProductCategory.category_id,
@@ -87,35 +96,53 @@ class ProductCategoryActions:
                 Product.price,
                 Product.quantity_available,
                 Product.status,
-                Status.status_name
-            ).join(ProductCategory)
+                Status.status_name,
+            )
+            .join(ProductCategory)
             .join(Product, on=(ProductCategoryRef.product_id == Product.product_id))
             .join(Status, on=(Product.status == Status.row_id))
-            .where(ProductCategoryRef.category_id == category_id)
+            .where(
+                (ProductCategoryRef.category_id == category_id)
+                & (
+                    Product.name.contains(params.name)
+                    if params.name
+                    else True
+                )
+            )
+            .order_by(*sort_by)
+            .limit(params.limit)
+            .offset(params.skip)
             .dicts()
         )
+        print(__rows.sql())
         return [row for row in __rows]
 
     @staticmethod
     def add_prod_category(cat_prod_ref: IProductCategoryRef, current_user: int):
         rows = ProductCategoryRef.select().where(
-            (ProductCategoryRef.product_id == cat_prod_ref.product_id) &
-            (ProductCategoryRef.category_id == cat_prod_ref.category_id)
+            (ProductCategoryRef.product_id == cat_prod_ref.product_id)
+            & (ProductCategoryRef.category_id == cat_prod_ref.category_id)
         )
         rows_ = [row for row in rows]
         if len(rows_):
             raise ValueError("product already in category")
         ProductCategoryRef.insert(**cat_prod_ref.dict()).execute()
-        return {"add": (f"Prod: {cat_prod_ref.product_id} === Cat: {cat_prod_ref.category_id}")}
+        return {
+            "add": (
+                f"Prod: {cat_prod_ref.product_id} === Cat: {cat_prod_ref.category_id}"
+            )
+        }
 
     @staticmethod
     def remove_prod_from_category(cat_prod_ref: IProductCategoryRef, current_user: int):
         rows = ProductCategoryRef.select().where(
-            (ProductCategoryRef.product_id == cat_prod_ref.product_id) &
-            (ProductCategoryRef.category_id == cat_prod_ref.category_id)
+            (ProductCategoryRef.product_id == cat_prod_ref.product_id)
+            & (ProductCategoryRef.category_id == cat_prod_ref.category_id)
         )
         rows_ = [row for row in rows]
         if not len(rows_):
             raise ValueError("product not in category")
         rows[0].delete_instance()
-        return {"removed": f"Prod: {cat_prod_ref.product_id} === Cat: {cat_prod_ref.category_id}"}
+        return {
+            "removed": f"Prod: {cat_prod_ref.product_id} === Cat: {cat_prod_ref.category_id}"
+        }
